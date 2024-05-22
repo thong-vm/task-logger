42d4a5d6ebc06b284639115aad4cd806077a289d
thong-vm <thongvm@sync-partners.com>
2024-05-22 17:51:52 +0900

    update Panel.js

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -4,17 +4,21 @@ function Panel({ project }) {
   ));
   return (
     <div className="h-full">
-      <div className="flex flex-row  h-full ">
-        <div className="border-x h-full w-[50%] flex flex-col">
-          <span>Task</span>
-          <div className="mt-16 flex flex-col"> {tasks}</div>
-          <button className="text-sky-600 hover:text-sky-400 hover:underline">
-            + Add task
-          </button>
-        </div>
-        <div className="border-x h-full w-[25%]">Assigned</div>
-        <div className="border-x h-full w-[25%]">Progress</div>
-      </div>
+      <table>
+        <th className="text-2xl font-semibold mb-4 ">
+          <td>Task</td>
+          <td>Assigned</td>
+          <td>Progress</td>
+        </th>
+        {project.tasks.map((task) => (
+          <tr>
+            <td>{task.title}</td>
+            <td>{task.assigned}</td>
+            <td>{task.progress}</td>
+          </tr>
+        ))}
+        <button className="text-sky-600 hover:text-sky-400">+Add task</button>
+      </table>
     </div>
   );
 }
